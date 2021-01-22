eee728872abf0ca0585dc84e62d8b0107ca66a7a
meiyunxia <meiyunxia@jd.com>
2021-01-23 01:25:46 +0800

    gogogo

---
+++ netty/src/main/java/com/ggmc/netty/io/bio/SocketClient.java
@@ -0,0 +1,23 @@
+package com.ggmc.netty.io.bio;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * @author meiyunxia
+ * @date 2021/1/16  11:23 上午
+ */
+public class SocketClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 9000);
+        //向服务端发送数据
+        socket.getOutputStream().write("HelloServer".getBytes());
+        socket.getOutputStream().flush();
+        System.out.println("向服务端发送数据结束");
+        byte[] bytes = new byte[1024];
+        //接收服务端回传的数据
+        socket.getInputStream().read(bytes);
+        System.out.println("接收到服务端的数据：" + new String(bytes));
+        socket.close();
+    }
+}
